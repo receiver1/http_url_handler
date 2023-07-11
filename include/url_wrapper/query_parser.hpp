986de5ba7cbe1f6849ac25e6728561859ba21026
neovstan <neovstan@gmail.com>
2023-07-11 21:56:17 +0300

    CMake, bug fixes, clang-format, vcpkg

---
+++ include/url_wrapper/query_parser.hpp
@@ -0,0 +1,39 @@
+#ifndef URL_WRAPPER_QUERY_PARSER_HPP
+#define URL_WRAPPER_QUERY_PARSER_HPP
+
+#include <string>
+#include <unordered_map>
+
+namespace net {
+namespace http {
+inline std::unordered_map<std::string, std::string> parse_query(const std::string& query) {
+  std::unordered_map<std::string, std::string> out{};
+
+  bool pair_complete{};
+  auto beg_it = query.begin() + (query[0] == '?' ? 1 : 0);
+  std::pair<std::string, std::string> pair{};
+  for (auto it = query.begin(); it != query.end(); ++it) {
+    if (*it == '=') {
+      pair.first = std::string{beg_it, it};
+      beg_it = it + 1;
+    } else if (*it == '&') {
+      pair.second = std::string{beg_it, it};
+      beg_it = it + 1;
+      pair_complete = true;
+    } else if (it == (query.end() - 1)) {
+      pair.second = std::string{beg_it, query.end()};
+      pair_complete = true;
+    }
+
+    if (pair_complete) {
+      out.emplace(pair);
+      pair_complete = false;
+    }
+  }
+
+  return out;
+}
+}  // namespace http
+}  // namespace net
+
+#endif  // URL_WRAPPER_QUERY_PARSER_HPP
---
+++ include/url_wrapper/url_parser.h
@@ -0,0 +1,41 @@
+#ifndef URL_WRAPPER_URL_PARSER_H
+#define URL_WRAPPER_URL_PARSER_H
+
+#include <string>
+
+class http_parser_url;
+
+namespace net {
+namespace http {
+class url_parser {
+  std::string url_;
+  http_parser_url *parser_;
+
+ public:
+  url_parser(const std::string &url_parser);
+  ~url_parser();
+
+  std::string schema() const;
+  std::string host() const;
+  std::uint16_t port() const;
+  std::string path() const;
+  std::string query() const;
+  std::string fragment() const;
+  std::string user_info() const;
+
+  const std::string &str() const { return url_; }
+  operator const std::string() { return url_; }
+  operator const char *() { return url_.c_str(); }
+
+  bool has_schema() const;
+  bool has_host() const;
+  bool has_port() const;
+  bool has_path() const;
+  bool has_query() const;
+  bool has_fragment() const;
+  bool has_user_info() const;
+};
+}  // namespace http
+}  // namespace net
+
+#endif  // URL_WRAPPER_URL_PARSER_H
