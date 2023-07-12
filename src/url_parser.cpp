c7c05ac7c6b23e9b4e9cc4495cd75170de22cb3d
Receiver <62743649+Receiver1@users.noreply.github.com>
2023-07-12 06:05:11 -0700

    Update url_parser.cpp

--- src/url_parser.cpp
+++ src/url_parser.cpp
@@ -2,34 +2,59 @@
 
 #include <http_parser.h>
 
-static bool has_field(http_parser_url* const parser, const http_parser_url_fields field) {
+static bool has_field(http_parser_url *const parser,
+                      const http_parser_url_fields field) {
   return static_cast<bool>(parser->field_set & (1 << field));
 }
 
-static std::string get_field(http_parser_url* const parser, const http_parser_url_fields field,
-                             const std::string& url_parser) {
+static std::string get_field(http_parser_url *const parser,
+                             const http_parser_url_fields field,
+                             const std::string &url_parser) {
   auto it = url_parser.begin() + parser->field_data[field].off;
   return std::string{it, it + parser->field_data[field].len};
 }
 
-net::http::url_parser::url_parser(const std::string& url_parser)
-    : url_{url_parser}, parser_{new http_parser_url} {
-  http_parser_url_init(parser_);
-  const auto error = http_parser_parse_url(url_parser.c_str(), url_parser.size(), false, parser_);
-  if (error) throw http_errno_name(http_errno(error));
+net::http::url_parser::url_parser(url_parser &&other) noexcept {
+  this->swap(other);
 }
 
-net::http::url_parser::~url_parser() { delete parser_; }
+net::http::url_parser::url_parser(const url_parser &other) noexcept
+    : parser_{new http_parser_url} {
+  url_ = other.url_;
+  std::memcpy(parser_, other.parser_, sizeof http_parser_url);
+}
 
-std::string net::http::url_parser::schema() const { return get_field(parser_, UF_SCHEMA, url_); }
+net::http::url_parser::url_parser(const std::string &url)
+    : url_{url}, parser_{new http_parser_url} {
+  http_parser_url_init(parser_);
+  const auto error =
+      http_parser_parse_url(url.c_str(), url.size(), false, parser_);
+  if (error)
+    throw http_errno_name(http_errno(error));
+}
 
-std::string net::http::url_parser::host() const { return get_field(parser_, UF_HOST, url_); }
+net::http::url_parser::~url_parser() {
+  if (parser_)
+    delete parser_;
+}
+
+std::string net::http::url_parser::schema() const {
+  return get_field(parser_, UF_SCHEMA, url_);
+}
+
+std::string net::http::url_parser::host() const {
+  return get_field(parser_, UF_HOST, url_);
+}
 
 std::uint16_t net::http::url_parser::port() const { return parser_->port; }
 
-std::string net::http::url_parser::path() const { return get_field(parser_, UF_PATH, url_); }
+std::string net::http::url_parser::path() const {
+  return get_field(parser_, UF_PATH, url_);
+}
 
-std::string net::http::url_parser::query() const { return get_field(parser_, UF_QUERY, url_); }
+std::string net::http::url_parser::query() const {
+  return get_field(parser_, UF_QUERY, url_);
+}
 
 std::string net::http::url_parser::fragment() const {
   return get_field(parser_, UF_FRAGMENT, url_);
@@ -39,16 +64,54 @@ std::string net::http::url_parser::user_info() const {
   return get_field(parser_, UF_USERINFO, url_);
 }
 
-bool net::http::url_parser::has_schema() const { return has_field(parser_, UF_SCHEMA); }
+bool net::http::url_parser::has_schema() const {
+  return has_field(parser_, UF_SCHEMA);
+}
 
-bool net::http::url_parser::has_host() const { return has_field(parser_, UF_HOST); }
+bool net::http::url_parser::has_host() const {
+  return has_field(parser_, UF_HOST);
+}
 
-bool net::http::url_parser::has_port() const { return has_field(parser_, UF_PORT); }
+bool net::http::url_parser::has_port() const {
+  return has_field(parser_, UF_PORT);
+}
 
-bool net::http::url_parser::has_path() const { return has_field(parser_, UF_PATH); }
+bool net::http::url_parser::has_path() const {
+  return has_field(parser_, UF_PATH);
+}
 
-bool net::http::url_parser::has_query() const { return has_field(parser_, UF_QUERY); }
+bool net::http::url_parser::has_query() const {
+  return has_field(parser_, UF_QUERY);
+}
 
-bool net::http::url_parser::has_fragment() const { return has_field(parser_, UF_FRAGMENT); }
+bool net::http::url_parser::has_fragment() const {
+  return has_field(parser_, UF_FRAGMENT);
+}
 
-bool net::http::url_parser::has_user_info() const { return has_field(parser_, UF_USERINFO); }
+bool net::http::url_parser::has_user_info() const {
+  return has_field(parser_, UF_USERINFO);
+}
+
+void net::http::url_parser::swap(url_parser &other) noexcept {
+  std::swap(url_, other.url_);
+  std::swap(parser_, other.parser_);
+}
+
+net::http::url_parser &
+net::http::url_parser::operator=(url_parser &&other) noexcept {
+  if (this != &other)
+    this->swap(other);
+  return *this;
+}
+
+net::http::url_parser &
+net::http::url_parser::operator=(const url_parser &other) noexcept {
+  if (this == &other)
+    return *this;
+  url_ = other.url_;
+  if (parser_ != nullptr)
+    delete parser_;
+  parser_ = new http_parser_url;
+  std::memcpy(parser_, other.parser_, sizeof http_parser_url);
+  return *this;
+}
